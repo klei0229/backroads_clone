45cc9ec8525925d6932a1676254eae4541abe0dc
root <root@DESKTOP-9670AL5.localdomain>
2023-04-21 10:22:53 -0400

    navbar content done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,48 @@
+import logo from "../../images/backroads-logo.svg";
+const links = [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Services", link: "#services" },
+  { name: "Tours", link: "#tours" },
+];
+const socials = [
+  { name: "facebook", link: "https://facebook.com", icon: "" },
+  { name: "twitter", link: "https://twitter.com", icon: "" },
+  { name: "sqaure", link: "https://squarespace", icon: "" },
+];
+
 const Navbar = () => {
-    return (
-        <>
-        <h1>navbar</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Navbar</h1>
+      {/* LOGO */}
+      <img src={logo} />
+
+      {/* LINKS */}
+      <ul>
+        {links.map((link) => {
+          return (
+            <li>
+              <a href={link.link}>{link.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* Socials */}
+      <ul>
+        {socials.map((social) => {
+          return (
+            <li>
+              <a href={social.link}>
+                <img src={logo} />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default Navbar;
